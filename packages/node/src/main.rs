eaa8b6e7b58224b52b1e236a127f8ed7d9f52ae5
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-08-12 19:27:03 +0100

    feat: command macro

--- packages/node/src/main.rs
+++ packages/node/src/main.rs
@@ -1,11 +1,32 @@
-use bollard::Docker;
-use eyre::{Ok, Result};
+use eyre::Result;
+use futures_util::{future, SinkExt, StreamExt, TryStreamExt};
+use tokio::{net::TcpListener, spawn};
+use tokio_tungstenite::{accept_async, tungstenite::Message};
+
+pub mod server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let docker = Docker::connect_with_defaults()?;
+    let listener = TcpListener::bind("0.0.0.0:9000").await?;
 
-    let version = docker.version().await?;
-    println!("{:?}", version);
+    while let Ok((socket, addr)) = listener.accept().await {
+        println!("Accepting {}", addr);
+
+        spawn(async move {
+            let websocket = accept_async(socket).await.unwrap();
+            let (mut write, read) = websocket.split();
+
+            let mut rx = read.try_filter(|msg| future::ready(msg.is_binary() || msg.is_text()));
+            while rx.next().await.is_some_and(|r| r.is_ok()) {
+                write
+                    .send(Message::Text("Hello, World!".into()))
+                    .await
+                    .unwrap();
+            }
+
+            println!("{} disconnected", addr);
+        });
+    }
+
     return Ok(());
 }
