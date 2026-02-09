c4865a293f5eb91169372be8de7987b211032627
Brooke <46959407+brooke-ec@users.noreply.github.com>
2026-02-09 18:31:49 +0000

    feat: switch to HTTP and SSE

--- packages/node/src/server.rs
+++ packages/node/src/server.rs
@@ -1,17 +1,1 @@
-use eyre::Result;
-use luminary_macros::command;
-use serde_json::json;
 
-struct CommandContext {}
-
-fn main() -> Result<()> {
-    PingCommand { time: 42 };
-    let _ = ping(CommandContext {}, json!({"time": 42}));
-    return Ok(());
-}
-
-#[command]
-fn ping(_ctx: CommandContext, time: u64) -> Result<()> {
-    println!("Ping received! {:}", time);
-    return Ok(());
-}
