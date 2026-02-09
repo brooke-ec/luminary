49db1e3786ed8e86d9e67aeb0a4e09678504722b
Brooke <46959407+nimajnebec@users.noreply.github.com>
2026-02-09 19:18:04 +0000

    chore: add workspace conviniences

---
+++ justfile
@@ -0,0 +1,10 @@
+[parallel]
+dev: _dev_web _dev_node
+
+[working-directory: 'packages/node']
+_dev_node:
+    cargo watch -x run
+
+[working-directory: 'packages/panel']
+_dev_web:
+    pnpm vite dev
--- packages/node/src/main.rs
+++ packages/node/src/main.rs
@@ -1,17 +1,15 @@
 use axum::{routing::get, Router};
-use eyre::Result;
+use color_eyre::eyre::Result;
 use tokio::net::TcpListener;
 
 pub mod server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().nest(
-        "/api/",
-        Router::new().route("/ping", get(|| async { "pong" })),
-    );
+    let app = Router::new().nest("/api/", Router::new().route("/ping", get(|| async { "pong" })));
 
     let listener = TcpListener::bind("0.0.0.0:9000").await?;
+    println!("Listening on http://127.0.0.1:{}", listener.local_addr()?.port());
     axum::serve(listener, app).await?;
     return Ok(());
 }
