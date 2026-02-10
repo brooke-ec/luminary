cbe12ece0d00eaf3bfe23c099d23af5f50374248
Brooke <46959407+nimajnebec@users.noreply.github.com>
2026-02-10 09:00:32 +0000

    chore: setup structure

--- packages/node/src/main.rs
+++ packages/node/src/main.rs
@@ -1,15 +1,14 @@
-use axum::{routing::get, Router};
+use axum::Router;
 use color_eyre::eyre::Result;
 use tokio::net::TcpListener;
 
-pub mod server;
+mod api;
+mod docker;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().nest("/api/", Router::new().route("/ping", get(|| async { "pong" })));
-
     let listener = TcpListener::bind("0.0.0.0:9000").await?;
     println!("Listening on http://127.0.0.1:{}", listener.local_addr()?.port());
-    axum::serve(listener, app).await?;
+    axum::serve(listener, Router::new().nest("/api/", api::router())).await?;
     return Ok(());
 }
