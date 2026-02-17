5ac78380b4afdfbbb8a303c2ac69b754d2421f35
Brooke <46959407+brooke-ec@users.noreply.github.com>
2026-02-17 14:48:53 +0000

    feat: reading compose project

--- packages/node/src/core/project.rs
+++ packages/node/src/core/project.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 
 use bollard::query_parameters::ListContainersOptionsBuilder;
-use color_eyre::eyre::{Ok, Result, WrapErr};
+use color_eyre::eyre::{ContextCompat, Ok, Result, WrapErr};
+use docker_compose_types::Compose;
 use luminary_macros::wrap_err;
-use tokio::fs;
+use tokio::fs::{self, File};
 
 use crate::core::{
     LuminaryCore,
@@ -16,6 +17,7 @@ const COMPOSE_PROJECT_LABEL: &str = "com.docker.compose.project";
 const COMPOSE_SERVICE_LABEL: &str = "com.docker.compose.service";
 
 const ENV_PROJECT_DIRECTORY: &str = "LUMINARY_PROJECT_DIRECTORY";
+const COMPOSE_FILENAME: &str = "compose.yml";
 
 impl LuminaryCore {
     #[wrap_err("Failed to list projects from filesystem")]
@@ -29,8 +31,20 @@ impl LuminaryCore {
             .await
             .wrap_err("Failed to list project directory contents")?;
         while let Some(entry) = entries.next_entry().await? {
-            let path = entry.path();
-            // do something with path
+            let mut path = entry.path();
+            if path.is_dir() {
+                let project_name = path
+                    .file_name()
+                    .wrap_err("Failed to get project directory name")?
+                    .to_str()
+                    .wrap_err("Failed to read project directory name")?
+                    .to_owned();
+                path.push(COMPOSE_FILENAME);
+                if path.exists() {
+                    let file = File::open(path).await.wrap_err("Failed to open compose file")?;
+                    let compose: Compose = serde_saphyr::from_reader(file.into_std().await)?;
+                }
+            }
         }
 
         return Ok(projects);
