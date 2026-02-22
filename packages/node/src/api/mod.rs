72480bcf427a911a8891fc16d59061475e0e68a1
Brooke <46959407+brooke-ec@users.noreply.github.com>
2026-02-22 10:54:02 +0000

    feat: transmit subscribe over sse

--- packages/node/src/api/mod.rs
+++ packages/node/src/api/mod.rs
@@ -1,6 +1,24 @@
-use axum::Router;
+use std::convert::Infallible;
+
+use axum::response::Sse;
 use axum::routing::get;
+use axum::{Router, response::sse::Event};
+use futures_util::{Stream, StreamExt};
+
+use crate::ENGINE;
 
 pub fn router() -> Router {
-    Router::new().route("/ping", get(|| async { "pong" }))
+    Router::new()
+        .route("/ping", get(|| async { "pong" }))
+        .route("/containers", get(containers))
+}
+
+async fn containers() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    return Sse::new(
+        ENGINE
+            .get()
+            .unwrap()
+            .subscribe()
+            .map(|r| Ok(Event::default().data(serde_json::to_string(&r.unwrap()).unwrap()))),
+    );
 }
--- packages/node/src/main.rs
+++ packages/node/src/main.rs
@@ -1,29 +1,27 @@
 //! The main entry point for the Luminary Node, which serves as the backend for the Luminary Panel.
 
+use std::sync::OnceLock;
+
 use axum::Router;
 use dotenv::dotenv;
-use eyre::Result;
+use eyre::{Context, Result};
+use luminary_core::LuminaryEngine;
 use tokio::net::TcpListener;
 
-use futures_util::StreamExt;
-
 mod api;
+
+static ENGINE: OnceLock<LuminaryEngine> = OnceLock::new();
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+    let engine = LuminaryEngine::create().wrap_err("Error creating Luminary Engine")?;
+    ENGINE.set(engine).expect("Somehow, ENGINE was already set");
 
-    let engine = luminary_core::LuminaryEngine::default()?;
-    let mut stream = engine.subscribe();
+    let listener = TcpListener::bind("0.0.0.0:9000").await?;
+    let router = Router::new().nest("/api/", api::router());
 
-    while let Some(projects) = stream.next().await {
-        println!("{:#?}", projects?);
-    }
-
-    // let listener = TcpListener::bind("0.0.0.0:9000").await?;
-    // let router = Router::new().nest("/api/", api::router());
-
-    // println!("Listening on http://127.0.0.1:{}", listener.local_addr()?.port());
-    // axum::serve(listener, router).await?;
+    println!("Listening on http://127.0.0.1:{}", listener.local_addr()?.port());
+    axum::serve(listener, router).await?;
     return Ok(());
 }
