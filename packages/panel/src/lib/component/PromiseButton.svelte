9c34a1d4815155e1dfc489bf2972d7ede1e2a50c
Brooke <46959407+brooke-ec@users.noreply.github.com>
2026-03-16 19:45:52 +0000

    docs: promise button docs

--- packages/panel/src/lib/component/PromiseButton.svelte
+++ packages/panel/src/lib/component/PromiseButton.svelte
@@ -1,3 +1,27 @@
+<!--
+	@component
+	
+	A Svelte component for a button that runs a promise.
+
+	The button will be disabled while the promise is running, and will show a loading spinner.
+
+    The content of the button is determined by the children snippet. See the example below for usage:
+
+    # Example
+
+    ```svelte
+    <PromiseButton onclick={login}>
+			{#snippet children(loading)}
+				{#if loading}
+					Logging in...
+				{:else}
+					Log In
+				{/if}
+			{/snippet}
+		</PromiseButton>
+    ```	
+-->
+
 <script lang="ts">
 	import type { Snippet } from "svelte";
 
